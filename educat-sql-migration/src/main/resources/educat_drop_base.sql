5957cb81cc506654f58432392fb0a8cb8dabac32
Andrey Borisov <andreybrsv@ya.ru>
2019-07-02 15:46:35 +0300

    Отредактировал sql для sql-migration. Добавил educat-mvc модуль.

---
+++ educat-sql-migration/src/main/resources/educat_drop_base.sql
@@ -0,0 +1,21 @@
+-- Дроп всех таблиц для пересоздания
+
+drop table ec_user;
+
+drop table ec_user_roles;
+
+drop table ec_educational_courses;
+
+drop table ec_educational_tasks;
+
+drop table ec_user_complete_tasks;
+
+drop table ec_user_course_subscriptions;
+
+drop table ec_educational_achievements;
+
+drop table ec_articles;
+
+drop table ec_article_comments;
+
+drop table ec_user_bookmarked_articles;
--- educat-sql-migration/src/main/resources/educat_test_db_data.sql
+++ educat-sql-migration/src/main/resources/educat_test_db_data.sql
@@ -1,1 +1,27 @@
 -- Данные для тестовой базы данных
+
+-- *ПОЛЬЗОВАТЕЛИ*
+-- Роли
+INSERT INTO ec_user_roles (role_name, role_description) VALUES
+('admin', 'administration of educat'),
+('moderator', 'moderation of educat'),
+('company', 'front compnay of educat'),
+('tutor', 'tutors of educat'),
+('user', 'educator');
+
+-- Пользователи
+INSERT INTO ec_user (user_role_id, email, login, pass, first_name, second_name, status) VALUES
+            (1, 'andreybrsv@yandex.ru', 'AndreyBrsv', '12345', 'Andrey', 'Borisov', 'ACTIVE'),
+            (1, 'ilyamikheev@yandex.ru', 'MikheevShow', '12345', 'Ilya', 'Mikheev', 'ACTIVE'),
+            (1, 'khabibullinvasiliy@yandex.ru', 'GitKhab', '12345', 'Vasiliy', 'Khabibullin', 'ACTIVE'),
+            (2, 'moderator@yandex.ru', 'Moderator123', '123', 'Moder', 'Moderov', 'DISABLED'),
+            (3, 'super@vuz.ru', 'supervuz', '123', 'supervuz', 'supervuz', 'DELETED'),
+            (4, 'repetitor@yandex.ru', 'Repetitor123', '123', 'Petr', 'Petrov', 'ACTIVE'),
+            (5, 'student1@yandex.ru', 'User123', '123', 'Ivan', 'Ivanov', 'ACTIVE'),
+            (6, 'student2@yandex.ru', 'User321', '321', 'Sergey', 'Sergeev', 'BLOCKED');
+
+-- *ОБРАЗОВАТЕЛЬНЫЕ КУРСЫ*
+-- Образовательные курсы
+
+
+
