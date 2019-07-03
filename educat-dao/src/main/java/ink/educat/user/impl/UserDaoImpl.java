a7fd44937117065b90d1b8cc68a45fac14446dd3
Andrey Borisov <andreybrsv@ya.ru>
2019-07-03 15:42:50 +0300

    Реализация методов AbsctractDao в UserDaoImpl

--- educat-dao/src/main/java/ink/educat/user/impl/UserDaoImpl.java
+++ educat-dao/src/main/java/ink/educat/user/impl/UserDaoImpl.java
@@ -6,6 +6,7 @@ import ink.educat.user.api.Entities.UserRole;
 import ink.educat.user.api.Entities.UserStatus;
 import ink.educat.user.api.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.ManagedMap;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -14,6 +15,7 @@ import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class UserDaoImpl implements UserDao {
@@ -39,6 +41,20 @@ public class UserDaoImpl implements UserDao {
                 userRole
         );
     });
+
+
+    private final Map<String, String> userMapper(User user) {
+        Map<String, String> values = new ManagedMap<>();
+            values.put("user_id", Long.toString(user.getId()));
+            values.put("login", user.getLogin());
+            values.put("email", user.getEmail());
+            values.put("first_name", user.getFirstName());
+            values.put("second_name", user.getSecondName());
+            values.put("UserStatus", user.getUserStatus().getDisplayableName());
+            values.put("UserRole", user.getUserRole().getDisplayableName());
+
+        return values;
+    }
 
     @Autowired
     public UserDaoImpl(final JdbcTemplate jdbcTemplate) {
@@ -70,6 +86,7 @@ public class UserDaoImpl implements UserDao {
             return null;
         }
 
+        //TODO maybe .size() > 1?
         Preconditions.checkState(
                 userList.size() == 1,
                 "Found multiple users with this email!"
@@ -77,5 +94,127 @@ public class UserDaoImpl implements UserDao {
 
         return userList.iterator().next();
 
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nullable
+    public User findById(@NonNull final long id) {
+        Preconditions.checkArgument(
+                id > 0,
+                "ID must be > 0!");
+
+        final MapSqlParameterSource mapSqlParameterSource =
+                new MapSqlParameterSource().addValue("user_id", id);
+
+        final List<User> userList = jdbcTemplate.query(
+                "SELECT DISTINCT * FROM EC_USERS U \n" +
+                        "LEFT JOIN EC_USER_ROLES R ON U.USER_ROLE_ID = R.USER_ROLE_ID \n" +
+                        "WHERE ID = :user_id",
+                userRowMapper,
+                mapSqlParameterSource
+        );
+
+        if(userList.isEmpty()) {
+            return null;
+        }
+
+        Preconditions.checkState(
+                userList.size() == 1,
+                "Found multiple users with this id!"
+        );
+
+        return userList.iterator().next();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nullable
+    public List<User> findByIDs(@NonNull final Iterable<Long> ids) {
+        StringBuilder validIds = new StringBuilder();
+        Long id;
+
+        if(ids.iterator().hasNext()) {
+            id = ids.iterator().next();
+
+            if(id > 0) {
+                validIds.append(ids.iterator().next());
+            }
+        }
+
+        while (ids.iterator().hasNext()) {
+            id = ids.iterator().next();
+
+            if (id > 0) {
+                validIds.append(", " + ids.iterator().next());
+            }
+        }
+
+        Preconditions.checkArgument(
+                validIds.equals(""),
+                "No valid ids!"
+
+        );
+
+        final MapSqlParameterSource mapSqlParameterSource =
+                new MapSqlParameterSource().addValue("user_ids", validIds);
+
+        final List<User> userList = jdbcTemplate.query(
+                "SELECT DISTINCT * FROM EC_USERS U \n" +
+                        "LEFT JOIN EC_USER_ROLES R ON U.USER_ROLE_ID = R.USER_ROLE_ID \n" +
+                        "WHERE ID IN (:user_ids)",
+                userRowMapper,
+                mapSqlParameterSource
+        );
+
+        if(userList.isEmpty()) {
+            return null;
+        }
+
+        return userList;
+    }
+
+    //TODO ? сделать отдельный метод для валидации всех параметров. Либо это на клиенте?
+    //TODO ? использовать апдейт для одного юзера или есть спецаильный удобный апдейт всех?
+    /**
+     * {@inheritDoc}
+     */
+    public void saveOrUpdate(@NonNull final Iterable<User> users) {
+        while(users.iterator().hasNext())
+        {
+            saveOrUpdate(users.iterator().next());
+        }
+
+        //jdbcTemplate.batchUpdate();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void saveOrUpdate(@NonNull final User user) {
+        final MapSqlParameterSource mapSqlParameterSource =
+                new MapSqlParameterSource().addValues(userMapper(user));
+
+        jdbcTemplate.update(
+                "UPDATE EC_USERS SET " +
+                        "login = :login, email = :email, \n" +
+                        "first_name = :first_name, second_name = :second_name, \n" +
+                        "user_status = :user_status, user_role = :user_role \n" +
+                        "WHERE id = :user_id",
+                mapSqlParameterSource);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void delete(@NonNull final User user) {
+        final MapSqlParameterSource mapSqlParameterSource =
+                new MapSqlParameterSource().addValues(userMapper(user));
+
+        jdbcTemplate.queryForMap(
+                "DELETE FROM EC_USERS WHERE id = :id",
+                mapSqlParameterSource);
     }
 }
