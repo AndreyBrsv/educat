efa8056704a65cc2ab37dbe3645ae981740219b6
Ilya Mikheev <mikheev.show@gmail.com>
2019-07-03 23:09:53 +0300

    Added success message after server launch

--- educat-mvc/educat-mvc-web/src/main/java/TestServlet.java
+++ educat-mvc/educat-mvc-web/src/main/java/TestServlet.java
@@ -14,6 +14,6 @@ public class TestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Выполняется Get запрос");
+        resp.getWriter().write("If you see this inscription, it means Educat works!");
     }
 }
---
+++ educat-mvc/educat-mvc-web/src/main/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ivanpetroff
+  Date: 2019-07-03
+  Time: 22:58
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>hello</h1>
+</body>
+</html>
