b6453e73a406ea863a42ca8a754ae521ede7d67d
Ilya Mikheev <mikheev.show@gmail.com>
2019-07-07 23:28:04 +0300

    minor changes for heroku

--- educat-web/src/main/java/ink/educat/controllers/MainController.java
+++ educat-web/src/main/java/ink/educat/controllers/MainController.java
@@ -1,7 +1,5 @@
 package ink.educat.controllers;
 
-import ink.educat.service.user.api.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,21 +8,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    private final UserService userService;
-
-    @Autowired
-    public MainController(UserService userService) {
-        this.userService = userService;
-    }
-
 
     @RequestMapping(value="/", method=RequestMethod.GET)
     @ResponseBody
     public String addUser() {
 
-        userService.getUserByEmail("@");
-
-        return "{\"success\":1}";
+        return "{\"Если вы это видете, значит Educat работает \":1}";
     }
 
 
