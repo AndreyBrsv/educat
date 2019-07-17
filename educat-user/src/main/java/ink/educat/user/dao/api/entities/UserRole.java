c770d36e5144a0857083473d3ccfcd6b4127cb8f
Andrey Borisov <andreybrsv@ya.ru>
2019-07-18 01:52:38 +0300

    Переделал в Jsonable

--- educat-user/src/main/java/ink/educat/user/dao/api/entities/UserRole.java
+++ educat-user/src/main/java/ink/educat/user/dao/api/entities/UserRole.java
@@ -1,6 +1,9 @@
 package ink.educat.user.dao.api.entities;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.google.common.base.Preconditions;
+import ink.educat.core.dao.api.entities.Jsonable;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 /**
@@ -8,40 +11,45 @@ import org.checkerframework.checker.nullness.qual.NonNull;
  * @author Andrey Borisov
  */
 
-public enum UserRole {
-    ADMIN("Администратор", 1),
-    MODERATOR("Модератор", 2),
-    COMPANY("Компания", 3),
-    TUTOR("Тьютор", 4),
-    USER("Пользователь", 5);
+public enum UserRole implements Jsonable<UserRole> {
+    ADMIN("administrator", 1),
+    MODERATOR("moderator", 2),
+    COMPANY("company", 3),
+    TUTOR("tutor", 4),
+    USER("user", 5);
 
-    private String displayableName;
+    private String jsonValue;
     private int code;
 
-    UserRole(final String displayableName, final int code) {
-        this.displayableName = displayableName;
+    UserRole(final String jsonValue, final int code) {
+        this.jsonValue = jsonValue;
         this.code = code;
     }
 
+    //todo уже не нужно, так как есть parseByJsonValue? Аналогично и в UserStatus
     @NonNull
-    public static UserRole parseByName(@NonNull final String name) {
-        //noinspection ConstantConditions
-        Preconditions.checkArgument(
-                name != null,
-                "UserRoles name can't be empty!");
-        for (final UserRole role : UserRole.values()) {
-            if (role.name().equals(name)) {
-                return role;
-            }
-        }
+//    public static UserRole parseByName(@NonNull final String name) {
+//        //noinspection ConstantConditions
+//        Preconditions.checkArgument(
+//                name != null,
+//                "UserRoles name can't be empty!");
+//        for (final UserRole role : UserRole.values()) {
+//            if (role.name().equals(name)) {
+//                return role;
+//            }
+//        }
+//
+//        throw new IllegalArgumentException("Incorrect UserRole name!");
+//    }
 
-        throw new IllegalArgumentException("Incorrect UserRole name!");
+    @JsonCreator
+    public static UserRole parseByJsonValue(@NonNull final String jsonValue) {
+        return Jsonable.parseByJsonValue(jsonValue, UserRole.class);
     }
 
-    @NonNull
-    public String getDisplayableName() {
-        return displayableName;
-    }
+    @JsonValue
+    @Override
+    public String getJsonValue() { return jsonValue; }
 
     public int getCode() { return code; }
 }
--- educat-user/src/main/java/ink/educat/user/dao/api/entities/UserStatus.java
+++ educat-user/src/main/java/ink/educat/user/dao/api/entities/UserStatus.java
@@ -1,41 +1,52 @@
 package ink.educat.user.dao.api.entities;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.google.common.base.Preconditions;
+import ink.educat.core.dao.api.entities.Jsonable;
+import ink.educat.user.service.api.UserService;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 /**
  * Перечисление, отражающее статусы доступности пользователей.
  * @author Ilya Mikheev
  */
-public enum  UserStatus {
+public enum UserStatus implements Jsonable<UserStatus> {
 
-    ACTIVE("Активный"),
-    BLOCKED("Заблокирован"),
-    DELETED("Удален");
+    ACTIVE("active", "Активный аккаунт"),
+    BLOCKED("blocked", "Заблокированный аккаунт"),
+    DELETED("deleted", "Удаленный аккаунт");
 
-    private String displayableName;
+    private String jsonValue;
+    private String description;
 
-    UserStatus(final String displayableName) {
-        this.displayableName = displayableName;
+    UserStatus(final String jsonValue, final String description) {
+        this.jsonValue = jsonValue;
+        this.description = description;
     }
 
-    @NonNull
-    public static UserStatus parseByName(@NonNull final String name) {
-        //noinspection ConstantConditions
-        Preconditions.checkArgument(
-                name != null,
-                "UserStatus name can't be empty!");
-        for (final UserStatus status : UserStatus.values()) {
-            if (status.name().equals(name)) {
-                return status;
-            }
-        }
+//    @NonNull
+//    public static UserStatus parseByName(@NonNull final String name) {
+//        //noinspection ConstantConditions
+//        Preconditions.checkArgument(
+//                name != null,
+//                "UserStatus name can't be empty!");
+//        for (final UserStatus status : UserStatus.values()) {
+//            if (status.name().equals(name)) {
+//                return status;
+//            }
+//        }
+//
+//        throw new IllegalArgumentException("Incorrect UserStatus name!");
+//    }
 
-        throw new IllegalArgumentException("Incorrect UserStatus name!");
+    @JsonCreator
+    public static UserStatus parseByJsonValue(@NonNull final String jsonValue) {
+        return Jsonable.parseByJsonValue(jsonValue, UserStatus.class);
     }
 
-    @NonNull
-    public String getDisplayableName() {
-        return displayableName;
-    }
+
+    @JsonValue
+    @Override
+    public String getJsonValue() { return jsonValue; }
 }
